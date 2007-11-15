326e698e3068de60111d63fbd5ce250a9e773db4
theglu <theglu@1e1cc2a3-b62a-0410-bc93-fb3b3b0a0737>
2007-11-15 18:48:43 +0000

    New GMAIL SUPPORT :)
    git-svn-id: http://svn.getfiregpg.org/firegpg@309 1e1cc2a3-b62a-0410-bc93-fb3b3b0a0737

--- content/cgmail2.js
+++ content/cgmail2.js
@@ -116,22 +116,641 @@ var cGmail2 = {
                         td.setAttribute("style","color: green;");
                         td.innerHTML = i18n.getString("GMailSOK") + " " + infos2; //"La première signature de ce mail est de testtest (testtest)
                     }
-//qZkfSe
-                    td.innerHTML = '<div class="X5Xvu" idlink=""><span class="" style="' + td.getAttribute("style") + '">' + td.innerHTML + '</span></div>';
 
-                    /*td.setAttribute("style","color: orange;");
-                    td.innerHTML = i18n.getString("GMailD"); //"Ce mail à été décrypté";*/
+
+
+                    var firstPosition = contenuMail.indexOf("-----BEGIN PGP MESSAGE-----");
+					var lastPosition = contenuMail.indexOf("-----END PGP MESSAGE-----");
+
+                    if (firstPosition != -1 && lastPosition != -1) {
+
+                        td.innerHTML = i18n.getString("GMailD");
+
+                         var tmpListener = new Object;
+                        tmpListener = null;
+                        tmpListener = new cGmail2.callBack(doc)
+                        td.addEventListener('click',tmpListener,true);
+                    }
+
+                    td.innerHTML = '<div class="X5Xvu" idlink=""><span class="" style="' + td.getAttribute("style") + '">' + td.innerHTML + '</span></div>';
 
                     boutonbox.insertBefore(td,boutonbox.childNodes[boutonbox.childNodes.length - 1]);
 
                 }
             }
 
+            //END OF THE TEST FOR MESSAGES.
+
+            //Test for compose buttons 'CoUvaf'
+            var listeTest = doc.getElementsByClassName('LlWyA');
+            var listeTest2 = doc.getElementsByClassName('CoUvaf');
+
+            listeTest = listeTest.concat(listeTest2);
+
+            for (var i = 0; i < listeTest.length; i++) {
+
+                if (listeTest[i].hasAttribute("gpg") == false) {
+
+                    listeTest[i].setAttribute("gpg","ok");
+
+                    //Position to add the button
+                    var spamLimite = listeTest[i].getElementsByTagName('span');
+                    spamLimite = spamLimite[0];
+
+                    if (cGmail2.b_sign == true)
+                        this.addBouton(listeTest[i],doc,i18n.getString("GMailS"),"sign",spamLimite);
+                    if (cGmail2.b_sign_s == true)
+                        this.addBouton(listeTest[i],doc,i18n.getString("GMailSS"),"sndsign",spamLimite);
+                    if (cGmail2.b_crypt == true)
+                        this.addBouton(listeTest[i],doc,i18n.getString("GMailC"),"crypt",spamLimite);
+                    if (cGmail2.b_crypt_s == true)
+                        this.addBouton(listeTest[i],doc,i18n.getString("GMailCS"),"sndcrypt",spamLimite);
+                    if (cGmail2.b_signcrypt == true)
+                        this.addBouton(listeTest[i],doc,i18n.getString("GMailSAC"),"signcrypt",spamLimite);
+                    if (cGmail2.b_signcrypt_s == true)
+                        this.addBouton(listeTest[i],doc,i18n.getString("GMailSACS"),"sndsigncrypt",spamLimite);
+
+                    try {
+
+                        var tmpListener = new Object;
+                        tmpListener = null;
+                        tmpListener = new cGmail2.callBack(doc)
+                        listeTest[i].addEventListener('click',tmpListener,true);
+
+                    } catch (e) {}
+
+
+                }
+            }
+            //END OF THE TEST FOR COMPOSE BUTTONS
+
             setTimeout("cGmail2.checkDoc("+id+")", 5000);
 
 
         }
     },
+
+    //Function to intercepd clicks on buttons
+    callBack: function(doc) {
+
+        this._doc = doc;
+
+		this.handleEvent = function(event) { // Function in the function for handle... events.
+
+			var i18n = document.getElementById("firegpg-strings");
+
+            //If the user want to decrypt the mail (can use normal attibutes)
+			if (event.target.innerHTML == i18n.getString("GMailD")) {
+
+                var tmpNode = event.target;
+
+                var mailNode = null;
+
+                tmpNode = tmpNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;
+
+                var tmpNode = tmpNode.getElementsByTagName('div');
+
+
+                for (var i = 0; i < tmpNode.length; i++) {
+
+                    if (tmpNode[i].hasAttribute("gpg") == true) {
+                        mailNode = tmpNode[i];
+                        break;
+                    }
+                }
+
+                if (mailNode == null)
+                    return;
+
+				contenuMail = cGmail2.getMailContent(mailNode, this._doc);
+
+				reg=new RegExp("\\- \\-\\-\\-\\-\\-BEGIN PGP MESSAGE\\-\\-\\-\\-\\-", "gi"); // We don't have to detect disabled balises
+				contenuMail = contenuMail.replace(reg, "FIREGPGTRALALABEGINHIHAN");
+
+				reg=new RegExp("\\- \\-\\-\\-\\-\\-END PGP MESSAGE\\-\\-\\-\\-\\-", "gi"); // We don't have to detect disabled balises
+				contenuMail = contenuMail.replace(reg, "FIREGPGTRALALAENDHIHAN");
+
+				var firstPosition = contenuMail.indexOf("-----BEGIN PGP MESSAGE-----");
+				var lastPosition = contenuMail.indexOf("-----END PGP MESSAGE-----");
+
+				reg=new RegExp("FIREGPGTRALALABEGINHIHAN", "gi"); // We don't have to detect disabled balises
+				contenuMail = contenuMail.replace(reg, "-----BEGIN PGP MESSAGE-----");
+
+				reg=new RegExp("FIREGPGTRALALAENDHIHAN", "gi"); // We don't have to detect disabled balises
+				contenuMail = contenuMail.replace(reg, "-----END PGP MESSAGE-----");
+
+				contenuMail = contenuMail.substring(firstPosition,lastPosition + ("-----END PGP MESSAGE-----").length);
+
+
+				var password = getPrivateKeyPassword();
+				var result = GPG.baseDecrypt(contenuMail,password);
+				var crypttext = result.output;
+				var sdOut2 = result.sdOut2;
+				result = result.sdOut;
+
+				if (password == null)
+					return;
+
+				// If the crypt failled
+				if (result == "erreurPass") {
+					alert(i18n.getString("decryptFailedPassword"));
+					eraseSavedPassword();
+				}
+				else if (result == "erreur") {
+					alert(i18n.getString("decryptFailed"));
+				}
+				else
+				{
+					var signAndCryptResult = undefined;
+
+					//If there was a sign with the crypted text
+					if (result == "signValid")
+					{
+						infos = sdOut2.split(" ");
+						signAndCryptResult = "";
+						for (var ii = 1; ii < infos.length; ++ii)
+						{  signAndCryptResult = signAndCryptResult + infos[ii] + " ";}
+					}
+
+					showText(crypttext,undefined,undefined,undefined,signAndCryptResult);
+				}
+			}
+			else if (event.target.getAttribute('gpg_action') == "sndsign" || event.target.getAttribute('gpg_action') == "sign")
+			{
+
+				var mailContent = cGmail2.getWriteMailContent(this._doc,event.target.parentNode);
+
+
+				if (mailContent == "")
+					return;
+
+				var keyID = getSelfKey();
+				var password = getPrivateKeyPassword();
+
+
+				if (password == null || keyID == null)
+					return;
+
+				var result = GPG.baseSign(mailContent,password,keyID);
+
+						// If the sign failled
+				if(result.sdOut == "erreur") {
+					// We alert the user
+					alert(i18n.getString("signFailed"));
+				}
+				else if(result.sdOut == "erreurPass") {
+						alert(i18n.getString("signFailedPassword"));
+						eraseSavedPassword();
+				}
+				else {
+
+					cGmail2.setWriteMailContent(this._doc,event.target.parentNode,result.output);
+
+					if (event.target.getAttribute('gpg_action') == "sndsign")
+					{
+						cGmail2.sendEmail(event.target.parentNode,this._doc);
+					}
+				}
+
+			}
+			else if (event.target.getAttribute('gpg_action') == "sndcrypt" || event.target.getAttribute('gpg_action') == "crypt")
+			{
+
+				//This code has to mix with the previous else/if block
+				var mailContent = cGmail2.getWriteMailContent(this._doc,event.target.parentNode);
+
+				var whoWillGotTheMail = cGmail2.getToCcBccMail(this._doc,event.target.parentNode);
+
+
+				if (mailContent == "")
+					return;
+
+
+				var keyID = choosePublicKey(whoWillGotTheMail);
+
+				if (keyID == null || keyID == "")
+					return;
+
+				var result = GPG.baseCrypt(mailContent,keyID);
+
+						// If the sign failled
+				if(result.sdOut == "erreur") {
+					// We alert the user
+					alert(i18n.getString("cryptFailed"));
+				}
+				else {
+
+					cGmail2.setWriteMailContent(this._doc,event.target.parentNode,result.output);
+
+					if (event.target.getAttribute('gpg_action') == "sndcrypt")
+					{
+						cGmail2.sendEmail(event.target.parentNode,this._doc);
+					}
+
+				}
+			}
+			else if (event.target.getAttribute('gpg_action') == "sndsigncrypt" || event.target.getAttribute('gpg_action') == "signcrypt")
+			{
+
+				//This code has to mix with the previous else/if block
+				var mailContent = cGmail2.getWriteMailContent(this._doc,event.target.parentNode);
+
+				var whoWillGotTheMail = cGmail2.getToCcBccMail(this._doc,event.target.parentNode);
+
+
+				if (mailContent == "")
+					return;
+
+
+				var keyID = choosePublicKey(whoWillGotTheMail);
+
+				if (keyID == null || keyID == "")
+					return;
+
+				// Needed for a sign
+				var keySignID = getSelfKey();
+				if(keySignID == null)
+					return;
+
+				var password = getPrivateKeyPassword();
+				if(password == null)
+					return;
+
+				var result = GPG.baseCryptAndSign(mailContent, keyID,false,password, keySignID);
+
+						// If the sign failled
+				if(result.sdOut == "erreur") {
+					// We alert the user
+					alert(i18n.getString("cryptAndSignFailed"));
+				}
+				if(result.sdOut == "erreurPass") {
+					// We alert the user
+					eraseSavedPassword();
+					alert(i18n.getString("cryptAndSignFailedPass"));
+				}
+				else {
+
+					cGmail2.setWriteMailContent(this._doc,event.target.parentNode,result.output);
+
+					if (event.target.getAttribute('gpg_action') == "sndsigncrypt")
+					{
+						cGmail2.sendEmail(event.target.parentNode,this._doc);
+					}
+
+				}
+			}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+           /* if (event.target.getAttribute('gpg_action') == 'sign') {
+
+                //cGmail2.sendEmail(event.target.parentNode,this._doc);
+
+                cGmail2.setWriteMailContent(this._doc,event.target.parentNode,"coin");
+
+            }*/
+
+		};
+	},
+
+    addBouton: function(box,doc,label,action,spamLimite) {
+		var bouton = new Object;
+		bouton = null;
+		bouton = doc.createElement("button");
+
+		bouton.setAttribute("type","button");
+        bouton.setAttribute("gpg_action",action);
+
+        bouton.setAttribute("style","width: auto;");
+
+
+		bouton.innerHTML = label;
+
+		try {
+			box.insertBefore(bouton,spamLimite);
+
+
+		} catch (e) {}
+	},
+
+    /* Say to gmail that is time for send a mail ! */
+	sendEmail: function(nodeForScan, dDocument)
+	{
+
+		var children = nodeForScan.getElementsByTagName('button');
+
+
+			try {
+				var evt = dDocument.createEvent("MouseEvents");
+					evt.initEvent("click", true, true);
+					children[0].dispatchEvent(evt);
+			 } catch (e) { }
+
+	},
+
+    getWriteMailContent: function(dDocument,boutonxboxnode)
+	{
+
+			if (cGmail2.iframeOrTextarea(dDocument,boutonxboxnode) == "iframe")
+            {
+
+                var iframe = cGmail2.getTheIframe(dDocument,boutonxboxnode);
+
+
+                //First, we look for a selection
+                try { var select = iframe.contentWindow.getSelection(); }
+                catch (e) { var select = ""; }
+
+                //Autodetect
+                if (select.toString() == "")
+                {
+                    try { var select = iframe.contentWindow.document.body.innerHTML; }
+					catch (e) { var select = ""; }
+
+					if ( select != "")
+					{
+
+						var indexOfQuote = select.indexOf('<div class="gmail_quote">');
+						if (indexOfQuote == -1)
+							indexOfQuote = select.length;
+						contenuMail = Selection.wash(select.substring(0,indexOfQuote));
+
+						this.composeIndexOfQuote  = indexOfQuote;
+					}
+                }
+                else
+                {
+                    value = select.getRangeAt(0);
+
+
+                    var documentFragment = value.cloneContents();
+
+                    var s = new XMLSerializer();
+                    var d = documentFragment;
+                    var str = s.serializeToString(d);
+
+                    contenuMail = Selection.wash(str);
+
+                }
+
+
+            } else {
+
+                var textarea = cGmail2.getTheTextarea(dDocument,boutonxboxnode);
+
+                var select2 = "";
+				//Mode plain text
+
+                //Selection
+                try {
+					select2 = textarea.value;
+					select2 = select2.substring(textarea.selectionStart,textarea.selectionEnd);
+				} catch (e) { }
+
+
+                //Autodetect
+                if (select2 == "")
+				{
+                    select2 = textarea.value;
+
+                    var indexOfQuote = select2.indexOf("\n> ");
+                    select2 = select2.substring(0,indexOfQuote);
+
+                    indexOfQuote = select2.lastIndexOf("\n");
+
+                    if (indexOfQuote == -1)
+                    {
+                        select2 = textarea.value;
+                        indexOfQuote = select2.length;
+                    }
+
+                    textarea.selectionStart = 0;
+                    textarea.selectionEnd = indexOfQuote;
+
+                    contenuMail = Selection.wash(select2.substring(0,indexOfQuote));
+                }
+                else
+				{
+
+					contenuMail = Selection.wash(select2);
+				}
+
+
+            }
+
+
+			return contenuMail;
+	},
+
+    //Return iframe if the iframe is used to compose the mail, textarea if not.
+    iframeOrTextarea: function(dDocument,boutonxboxnode) {
+
+        var iframe = cGmail2.getTheIframe(dDocument,boutonxboxnode);
+        var textarea = cGmail2.getTheTextarea(dDocument,boutonxboxnode);
+
+        if (iframe == null || iframe.parentNode == undefined)
+            return "textarea";
+
+        if (textarea == null || textarea.parentNode == undefined)
+            return "iframe";
+
+        var style = iframe.parentNode.getAttribute('style');
+
+        if (style.indexOf('display: none;') != -1)
+            return "textarea";
+
+        return "iframe";
+
+
+    },
+
+    //Return the iframe used to compose a mail
+    getTheIframe: function(dDocument,boutonxboxnode) {
+
+        var tmp = boutonxboxnode;
+
+        tmp = tmp.parentNode;
+
+        tmp = tmp.childNodes[1];
+
+        tmp = tmp.firstChild;
+
+        tmp = tmp.childNodes[1];
+
+        tmp = tmp.childNodes[1];
+
+        tmp = tmp.getElementsByTagName('iframe');
+
+        tmp = tmp[0];
+
+        return tmp;
+
+
+    },
+
+    //Return the textarea used to compose a mail
+    getTheTextarea: function(dDocument,boutonxboxnode) {
+
+        var tmp = boutonxboxnode;
+
+        tmp = tmp.parentNode;
+
+        tmp = tmp.childNodes[1];
+
+        tmp = tmp.firstChild;
+
+        tmp = tmp.childNodes[1];
+
+        tmp = tmp.childNodes[1];
+
+        tmp = tmp.getElementsByTagName('textarea');
+
+        tmp = tmp[0];
+
+        return tmp;
+
+    },
+
+	getToCcBccMail: function(dDocument,boutonxboxnode) {
+		var forWho = "";
+		var tmpFor = "";
+
+        var tmp = boutonxboxnode;
+
+        tmp = tmp.parentNode;
+
+        tmp = tmp.childNodes[1];
+
+        tmp = tmp.firstChild;
+
+        var textareas = tmp .getElementsByTagName("textarea");
+
+
+        //On cherche la boite avec les boutons
+        for (var j = 0; j < textareas.length; j++) {
+            if (textareas[j].getAttribute("name") == "to" || textareas[j].getAttribute("name") == "cc" || textareas[j].getAttribute("name") == "bcc") {
+
+                forWho = forWho + " " + textareas[j].value;
+
+            }
+        }
+
+
+    	//Pattern
+		var reg = new RegExp('[a-zA-Z0-9._-]+@[a-zA-Z0-9-]+\.[a-zA-Z.]{2,5}', 'gi');
+
+		var aMail = reg.exec(forWho);
+
+		var i = 0;
+		var returnList = new Array();
+
+		while(aMail != null)
+		{
+			returnList[i] = aMail;
+			i++;
+			aMail = reg.exec(forWho);
+		}
+
+		return returnList;
+	},
+	setWriteMailContent: function(dDocument,boutonxboxnode,newText)
+	{
+
+
+
+
+        if (cGmail2.iframeOrTextarea(dDocument,boutonxboxnode) == "iframe")
+        {
+
+            var iframe = cGmail2.getTheIframe(dDocument,boutonxboxnode);
+
+
+            //First, we look for a selection
+            try { var select = iframe.contentWindow.getSelection(); }
+            catch (e) { var select = ""; }
+
+            //Autodetect
+            if (select.toString() == "")
+            {
+                try { var select = iframe.contentWindow.document.body.innerHTML; }
+                    catch (e) { var select = ""; }
+
+                    var reg=new RegExp("\n", "gi");
+                    newText = newText.replace(reg,"<br>");
+
+                iframe.contentWindow.document.body.innerHTML = newText + select.substring(this.composeIndexOfQuote, select.length) + "<br /><br />";
+            }
+            else
+            {
+                var reg=new RegExp("<", "gi");
+                newText = newText.replace(reg,"&lt;");
+
+                var reg=new RegExp("\n", "gi");
+                newText = newText.replace(reg,"<br>");
+
+                var range = select.getRangeAt(0);
+                var el = dDocument.createElement("div");
+
+                el.innerHTML = newText;
+
+                range.deleteContents();
+                range.insertNode(el);
+
+            }
+
+
+        } else {
+
+            var textarea = cGmail2.getTheTextarea(dDocument,boutonxboxnode);
+
+            var startPos = textarea.selectionStart;
+            var endPos = textarea.selectionEnd;
+            var chaine = textarea.value;
+
+            // We create the new string and replace it into the focused element
+            textarea.value = chaine.substring(0, startPos) + newText + chaine.substring(endPos, chaine.length);
+
+            // We select the new text.
+            textarea.selectionStart = startPos;
+            textarea.selectionEnd = startPos + newText.length ;
+
+        }
+
+
+	},
 
     //Retrun the content of a mail, need the div object with the mail
 	getMailContent: function(i,doc) {
@@ -200,19 +819,19 @@ var cGmail2 = {
 
             Array.prototype.inArray = function(val) {
 
-            for(var a2 = 0; a2 < this.length; ++a2) {
+                for(var a2 = 0; a2 < this.length; ++a2) {
 
-                if(this[ a2] == val){
+                    if(this[ a2] == val){
 
-                return true;
+                    return true;
+
+                    }
 
                 }
 
-            }
+                return false;
 
-            return false;
-
-        };
+            };
 
 
 		}
