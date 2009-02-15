7ce846ebaa3a9fbe5c419c10e9bf4e31de8f2068
theglu <theglu@1e1cc2a3-b62a-0410-bc93-fb3b3b0a0737>
2009-02-15 12:19:55 +0000

    0.7.4
    git-svn-id: http://svn.getfiregpg.org/firegpg@540 1e1cc2a3-b62a-0410-bc93-fb3b3b0a0737

--- content/Core/misc.js
+++ content/Core/misc.js
@@ -49,13 +49,13 @@ try {
 
 /* Constant: FIREGPG_VERSION
   The current version of FireGPG */
-const FIREGPG_VERSION = '0.7.3';
+const FIREGPG_VERSION = '0.7.4';
 
-const FIREGPG_VERSION_A = '073';
+const FIREGPG_VERSION_A = '074';
 
 /* Constant: FIREGPG_STATUS
   The status of the FireGPG's code. Can be _DEVEL_ or _RELASE_. Use _RELASE_ only for.. relases ;). */
-const FIREGPG_STATUS = 'DEVEL';
+const FIREGPG_STATUS = 'RELASE';
 
 /* Constant: FIREGPG_SVN
   The current subversion's revision number, for this file ! */
