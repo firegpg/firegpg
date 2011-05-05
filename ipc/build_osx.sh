0f2feaf5a3baf171a74770dbadaffa9de2bd5445
j <j@mailb.org>
2011-05-06 00:36:22 +0200

    Port Firegpg to Firefox 4.0 and later

---
+++ ipc/build_osx.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+name=firegpg
+MOZBUILD=$HOME/src/mozilla-central/obj-x86_64-apple-darwin10.6.0
+
+cd `dirname $0`
+IPC=`pwd`
+
+rm -rf $MOZBUILD/../extensions/$name-ipc
+cp -r $IPC/$name-ipc $MOZBUILD/../extensions/$name-ipc
+
+arch=i386
+cd $MOZBUILD/$arch
+../../build/autoconf/make-makefile extensions/$name-ipc
+cd $MOZBUILD/$arch/extensions/$name-ipc
+make
+
+arch=x86_64
+cd $MOZBUILD/$arch
+../../build/autoconf/make-makefile extensions/$name-ipc
+cd $MOZBUILD/$arch/extensions/$name-ipc
+make
+
+COMPONENTS=$IPC/components/
+mkdir -p $COMPONENTS
+
+lipo -create \
+ -arch i386 $MOZBUILD/i386/dist/bin/components/lib${name}_ipc.dylib \
+ -arch x86_64 $MOZBUILD/x86_64/dist/bin/components/lib${name}_ipc.dylib \
+ -output $COMPONENTS/lib${name}_ipc.dylib
+
+cp ../../dist/bin/components/${name}_ipc.xpt $COMPONENTS
